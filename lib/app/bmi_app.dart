4c893592e9eec5418ad7617f0771dfc0bc7d1df1
ahmed ramadan <ahmed.ramadan20@outlook.com>
2024-01-12 06:46:45 +0200

    first commit

---
+++ lib/app/bmi_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../screens/signin_screen.dart';
+
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignInPage(),
+
+
+    );
+  }
+}
